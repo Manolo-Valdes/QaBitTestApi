949c2efa9e7c9d3e0d0bac3f101cbad957b57376
Manolo-Valdes <nolis71cu@gmail.com>
2021-12-15 16:33:23 -0500

    fixing source tree

--- QaBitTestApiTests/Controllers/ProductsControllerTests.cs
+++ QaBitTestApiTests/Controllers/ProductsControllerTests.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 using QaBitTestApi.Db;
 using QaBitTestApi.Models;
 using System.Linq;
